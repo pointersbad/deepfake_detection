8be0e9654b06c0e0a13f9ba236c09e59a851c0b1
pointersbad <saliht12345@gmail.com>
2023-12-10 21:17:18 +0800

    feat: update the ensemble

--- test.py
+++ test.py
@@ -14,33 +14,32 @@ parser.add_argument('--path')
 # <filename: string> <target: 0 | 1>
 txt_path = parser.parse_args().path
 dataset = DeepFakeDataset(True, device, txt_path)
-loader = DataLoader(dataset, 1, True)
+loader = DataLoader(dataset)
 
 resnet = Network('wide_resnet101_2', device)
 resnet.load('warmup+cosine_annealing_warm_restarts_lr=0.002_e=96')
 resnet.eval()
-densenet1 = Network('densenet169', device)
-densenet1.load('warmup+cosine_annealing_warm_restarts_lr=0.002_e=96')
-densenet1.eval()
-densenet2 = Network('densenet169', device)
-densenet2.load('warmup+cosine_annealing_warm_restarts_lr=0.002_e=72')
-densenet2.eval()
+densenet = Network('densenet169', device)
+densenet.load('warmup+cosine_annealing_warm_restarts_lr=0.002_e=96')
+densenet.eval()
+efficientnet = Network('efficientnet_v2_s', device)
+efficientnet.load('warmup+multistep+swa_lr=0.008_e=27')
+efficientnet.eval()
+
 
 conf = [0, 0], [0, 0]
 targets, preds = [], []
-
 with torch.no_grad():
   for x, y in loader:
     x, y = (i.to(device) for i in (x, y))
     pred1 = resnet(x)
-    pred2 = densenet1(x)
-    pred3 = densenet2(x)
-    pred = 0.5 * pred1 + 0.3 * pred2 + 0.2 * pred3
+    pred2 = densenet(x)
+    pred3 = efficientnet(x)
+    pred = 0.4 * pred1 + 0.2 * pred2 + 0.4 * pred3
     for pred, y in zip(pred, y):
       preds.append(pred.detach().cpu())
       pred, y = (1 if x > 0.5 else 0 for x in (pred, y))
       targets.append(y)
       conf[pred][y] += 1
-
-print(f'AUC: {roc_auc_score(targets, preds)}')
 Trainer.metrics(conf)
+print(f'AUC: {roc_auc_score(targets, preds)}')
